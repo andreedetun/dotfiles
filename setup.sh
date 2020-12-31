12824e803196e70cc963d8d69eccb340515ba187
andreedetun <andre.edetun.falk@gmail.com>
2020-12-31 02:45:19 +0100

    Added a link to vim-plug

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 echo "Installing vim-plug"
 
+# https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
